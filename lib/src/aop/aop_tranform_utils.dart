8c59cc3f05386d43fd9ca88cc8cdae96f7359e8a
cpacm <shenliming1013@gmail.com>
2023-02-21 15:25:23 +0800

    fix:deepAstNodes

--- lib/src/aop/aop_tranform_utils.dart
+++ lib/src/aop/aop_tranform_utils.dart
@@ -193,6 +193,11 @@ class AopUtils {
       return TypedefType(node.typedefNode, Nullability.legacy,
           deepCopyASTNodes(node.typeArguments, ignoreGeneric: ignoreGenerics));
     }
+
+    if (node is InterfaceType) {
+      return InterfaceType(node.classNode, node.declaredNullability,
+          deepCopyASTNodes(node.typeArguments, ignoreGeneric: ignoreGenerics));
+    }
     return node;
   }
 }
