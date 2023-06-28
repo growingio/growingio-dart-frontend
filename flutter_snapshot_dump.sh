edcaa6331a68e10d8584fc2121c6016f60a4d5ff
shenliming <348515494@qq.com>
2023-06-28 15:55:24 +0800

    add flutter_snapshot_dump.sh

---
+++ flutter_snapshot_dump.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# 用于新版本Flutter的SNAPSHOT打包命令，请先按照以下步骤设置路径：
+# 1. 设置 Flutter 源码路径: FLUTTER_SOURCE_DIR=<PATH>
+# 2. 设置 Dart 源码路径: DART_SOURCE_DIR=<PATH>
+# 3. 在当前位置使用命令行运行: ./flutter_snapshot_dump <flutter_version> <dart_version> 
+#    比如 ./flutter_snapshot_dump 3.10.5 3.0.5
+# 4. 可以使用 git status 查看是否已经生成新的SNAPSHOT
+
+FLUTTER_SOURCE_DIR="/Users/shenliming/Program/flutter"
+DART_SOURCE_DIR="/Users/shenliming/Program/dart/sdk"
+
+if [ $# -ne 2 ]; then
+    echo "[flutter_snapshot_dump] 请输入正确的flutter版本号和Dart版本号."
+    exit 1
+fi
+
+source_dir=$PWD
+flutter_version="$1"
+dart_version="$2"
+if ! [[ "$flutter_version" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    echo "[flutter_snapshot_dump] Flutter版本号格式错误: $flutter_version"
+    exit 1
+fi
+
+if ! [[ "$dart_version" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    echo "[flutter_snapshot_dump] Dart版本号格式错误: $dart_version"
+    exit 1
+fi
+
+
+echo "[flutter_snapshot_dump] start checkout flutter tag with $flutter_version"
+cd "$FLUTTER_SOURCE_DIR"
+git pull
+git checkout -f "$flutter_version"
+
+echo "[flutter_snapshot_dump] start checkout dart tag with $dart_version"
+cd "$DART_SOURCE_DIR"
+git pull
+git checkout -f "$dart_version"
+
+echo "[flutter_snapshot_dump] start cherry-pick diff.patch"
+git apply "$source_dir/dart_flutter.patch"
+
+echo "[flutter_snapshot_dump] generate frontend_server.dart.snapshot"
+cd "$source_dir"
+dart pub get
+cd lib/flutter_frontend_server
+dart --deterministic --snapshot=frontend_server.dart.snapshot frontend_server_starter.dart
+echo "[flutter_snapshot_dump] COMPLETE!!!"
+
